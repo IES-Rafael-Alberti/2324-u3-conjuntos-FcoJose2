389eeae7c7c1a1227c6a8b8506153dc2a89d847a
Fco Jose Macho Delgado <fjmac@iesrafaelalberti.es>
2023-11-13 14:26:15 +0100

    3

--- src/Ejercicio5.py
+++ src/Ejercicio5.py
@@ -10,9 +10,24 @@ Encuentra la intersección entre los conjuntos pares y multiplos_de_tres y guár
 def numerosPares(numeros):
     pares = {}
     pares = set(pares)
-    for x in range(len(numeros), 2):
+    for x in range(0, len(numeros)+1, 2):
         pares.add(x)
     return pares
+
+def multiplo3(numeros):
+    multiplosDe3 = {}
+    multiplosDe3  = set(multiplosDe3)
+    for x in range(1,len(numeros)+1):
+        if x % 3 == 0:
+            multiplosDe3.add(x)
+    return multiplosDe3
+
+def intersecion(pares, multiplosDe3):
+    pares_y_multiplos_de_tres = pares & multiplosDe3
+    return pares_y_multiplos_de_tres
+
+
+
 
 
 
@@ -22,5 +37,9 @@ if __name__ == "__main__":
     numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
     #Proceso
     pares = numerosPares(numeros)
+    multiplosDe3 = multiplo3(numeros)
+    pares_y_multiplos_de_tres = intersecion(pares, multiplosDe3)
     #Salida
     print(pares)
+    print(multiplosDe3)
+    print(pares_y_multiplos_de_tres)
