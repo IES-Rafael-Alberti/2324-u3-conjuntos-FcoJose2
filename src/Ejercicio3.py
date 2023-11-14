2137be13f6171358cf5fba618e71cfebb1f62234
Fco Jose Macho Delgado <fjmac@iesrafaelalberti.es>
2023-11-14 07:53:05 +0100

    3

--- src/Ejercicio3.py
+++ src/Ejercicio3.py
@@ -11,10 +11,36 @@ conjunto_potencia({6, 1, 4})
 [set(), set([6]), set([1]), set([4]), set([6, 1]), set([6, 4]), set([1, 4]), set([6, 1, 4])]
 '''
 
+def obtener_domicilios_facturas(compras):
+    domicilios_facturas = set()
+    clientes_domicilios = {}
+
+    for compra in compras:
+        cliente, dia_del_mes, monto, domicilio = compra
+
+        if cliente not in clientes_domicilios:
+            clientes_domicilios[cliente] = set()
+
+        clientes_domicilios[cliente].add(domicilio)
+
+    for domicilios in clientes_domicilios.values():
+        domicilios_facturas.update(domicilios)
+
+    return list(domicilios_facturas)
+
 if __name__ == "__main__":
-    #Entrada
-    
-    #Proceso
-    
-    #Salida
-    
+
+    #entrada
+    compras = [
+    ("Nuria Costa", 5, 12780.78, "Calle Las Flores 355"),
+    ("Jorge Russo", 7, 699, "Mirasol 218"),
+    ("Nuria Costa", 7, 532.90, "Calle Las Flores 355"),
+    ("Julián Rodriguez", 12, 5715.99, "La Mancha 761"),
+    ("Jorge Russo", 15, 958, "Mirasol 218")
+    ]
+
+    #proceso
+    domicilios_para_facturas = obtener_domicilios_facturas(compras)
+
+    #salida
+    print(domicilios_para_facturas)
---
+++ test/test_1.py
@@ -0,0 +1,4 @@
+from src.Ejercicio1 import obtenerDomicilios
+
+def test_obtenerDomicilios():
+    assert obtenerDomicilios([("Nuria Costa", 5, 12780.78, "Calle Las Flores 355"), ("Jorge Russo", 7, 699, "Mirasol 218"), ("Nuria Costa", 7, 532.90, "Calle Las Flores 355"), ("Julián Rodriguez", 12, 5715.99, "La Mancha 761"), ("Jorge Russo", 15, 958, "Mirasol 218")]) == ['Calle Las Flores 355', 'Mirasol 218', 'La Mancha 761']
